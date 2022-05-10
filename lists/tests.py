f85c2e8fc52ed0c3efa220c04ca5846a5b80bc2c
fine-lab <18950670252@163.com>
2022-05-10 10:25:15 +0800

    before the refactor

--- lists/tests.py
+++ lists/tests.py
@@ -15,13 +15,21 @@ class SmokeTest(TestCase):
 		#response = home_page(request) # 2
 		response = self.client.get('/') # 1
 
-		html = response.content.decode('utf8') # 2
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
+		#html = response.content.decode('utf8') # 2
+		#self.assertTrue(html.startswith('<html>'))
+		#self.assertIn('<title>To-Do lists</title>', html)
+		#self.assertTrue(html.endswith('</html>'))
 
 		self.assertTemplateUsed(response, 'home.html') # 3
 
+	def test_uses_home_template(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
+
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
 #	def test_bad_maths(self):
 #		self.assertEqual(1 + 1, 3)
 		
