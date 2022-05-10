2e9b14e6df9fa4fc3227224ed3393875beadeb23
fine-lab <18950670252@163.com>
2022-05-10 14:25:18 +0800

    after the refator

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):		# (1)
 	def tearDown(self):
 		self.browser.quit()	# (3)
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self): # (2)
 
 		# Edit has heard about a cool new online to-do app. She goes
@@ -23,15 +28,9 @@ class NewVisitorTest(unittest.TestCase):		# (1)
 		self.assertIn('To-Do',header_text)
 
 		# She is invited to enter a to-do item straight away
-		#inputbox = self.browser.find_element_by_id('id_new_item')
-		#self.assertEqual(
-		#	inputbox.get_attribute('placeholder'),
-		#	'Enter a to-do item'
-		#)
 
 		# She types "Buy peacock feathers" into a text box (Edith's hobby
 		# is trying fly-fishing lures)
-		#inputbox.send_keys('Buy peacock feathers')
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -42,18 +41,23 @@ class NewVisitorTest(unittest.TestCase):		# (1)
 		# "1:Buy peacock feathers" as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		#self.assertTrue(
-		#	any(row.text == '1: Buy peacock feathers' for row in rows),
-		#	f"New to-do item did not appear in table. Contents were:\n{table.text}"
-		#)
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn(
-			'2: Use peacock feathers yo make a fly',
-			[row.text for row in rows]
-			)
+		#table = self.browser.find_element_by_id('id_list_table')
+		#rows = table.find_elements_by_tag_name('tr')
+
+		#self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		#self.assertIn(
+		#	'2: Use peacock feathers yo make a fly',
+		#	[row.text for row in rows]
+		#	)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('1: Use peacock feathers to make a fly')
 
 		# There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly" (Edith is very
